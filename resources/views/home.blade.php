d85c8a678d27bab47bb5add82be9a4d8f351cd6f
Gurvan <gurvancambray@gmail.com>
2024-01-09 13:55:35 +0100

    :construction: DASHBOARD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    test
     <div class="grid grid-cols-3 gap-5">
         <p>Panier: {{ !\Illuminate\Support\Facades\Session::get('rentables') ?: count(\Illuminate\Support\Facades\Session::get('rentables')) }}</p>
         @forelse($rentables as $rentable)
